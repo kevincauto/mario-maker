3c2509451cb93830a58e1c72b3b7654fbc605414
Kevin Cauto <kevincauto@aol.com>
2016-10-06 01:39:21 -0400

    Adding React-Bootstrap was a success.  I added in a navigation bar.  I began to structure my app a bit better.

---
+++ javascript/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navigation from './navigation.jsx';
+
+class App extends React.Component {
+  render() {
+    return (
+        <Navigation />
+        //<Body />
+        //<Footer />
+    )
+  }
+}
+
+export default App;
---
+++ javascript/world.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class World extends React.Component {
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
+
+
+//ReactDOM.render(<World />, document.getElementById('world'))
